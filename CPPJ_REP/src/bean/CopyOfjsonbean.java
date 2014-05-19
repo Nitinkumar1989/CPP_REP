a636d72a9caa6b8cfe894cf3275dc5686ee4f992
Vikas Kumar <vkumar@open-soft.net>
2014-05-19 09:04:54 +0530

    gr

---
+++ CPPJ_REP/src/bean/CopyOfjsonbean.java
@@ -0,0 +1,30 @@
+package bean;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CopyOfjsonbean {
+	
+	String name;
+	
+	int population;
+	private List<String> listofstate;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPopulation() {
+		return population;
+	}
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+	public List<String> getListofstate() {
+		return listofstate;
+	}
+	public void setListofstate(List<String> listofstate) {
+		this.listofstate = listofstate;
+	}
+}
